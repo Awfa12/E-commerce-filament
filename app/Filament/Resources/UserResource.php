b44463bfa441e34b6b734e4168b88061f6ae52be
Awfa12 <awfa.r1212@gmail.com>
2025-03-13 17:14:18 -0500

    Add Relation Manager

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\UserResource\Pages;
 use Filament\Resources\Pages\Page;
 use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\OrdersRelationManager;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -81,7 +82,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            OrdersRelationManager::class
         ];
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,10 @@ class User extends Authenticatable
         ];
     }
 
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
     /**
      * Get the user's initials
      */
