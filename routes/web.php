434a6d85e7367a0ecedb14d2642dd2b9034bfd04
Awfa12 <awfa.r1212@gmail.com>
2025-03-14 19:58:50 -0500

    All Front End Desing

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,28 @@
 <?php
 
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
+
 
 Route::get('/', HomePage::class)->name('home');
+Route::get('/categories', CategoriesPage::class)->name('categories');
+Route::get('/products', ProductsPage::class)->name('products');
+Route::get('/cart', CartPage::class)->name('cart');
+Route::get('/products/{product}', ProductDetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
 
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
 
 require __DIR__.'/auth.php';
